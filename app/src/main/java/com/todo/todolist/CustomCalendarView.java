0550dfe9cc1cbd2de95eeaf672b3c277949d4ef5
eanby00 <wjdwnstjd00@gmail.com>
2021-05-16 13:50:38 +0900

    0516

---
+++ app/src/main/java/com/todo/todolist/CustomCalendarView.java
@@ -0,0 +1,254 @@
+package com.todo.todolist;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+
+
+public class CustomCalendarView extends LinearLayout
+{
+    // for logging
+    private static final String LOGTAG = "Calendar View";
+
+    // how many days to show, defaults to six weeks, 42 days
+    private static final int DAYS_COUNT = 42;
+
+    // default date format
+    private static final String DATE_FORMAT = "MMMM yyyy";
+
+    // date format
+    private String dateFormat;
+
+    // current displayed month
+    private Calendar currentDate = Calendar.getInstance();
+
+    //event handling
+    private EventHandler eventHandler = null;
+
+    // internal components
+    private LinearLayout header;
+    private ImageView btnPrev;
+    private ImageView btnNext;
+    private TextView txtDate;
+    private GridView grid;
+
+    public CustomCalendarView(Context context)
+    {
+        super(context);
+    }
+
+    public CustomCalendarView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        initControl(context, attrs);
+    }
+
+    public CustomCalendarView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        initControl(context, attrs);
+    }
+
+    /**
+     * Load control xml layout
+     */
+    private void initControl(Context context, AttributeSet attrs)
+    {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.control_calendar, this);
+
+        loadDateFormat(attrs);
+        assignUiElements();
+        assignClickHandlers();
+
+        updateCalendar();
+    }
+
+    private void loadDateFormat(AttributeSet attrs)
+    {
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.CalendarView);
+
+        try
+        {
+            // try to load provided date format, and fallback to default otherwise
+            dateFormat = ta.getString(R.styleable.CalendarView_dateFormat);
+            if (dateFormat == null)
+                dateFormat = DATE_FORMAT;
+        }
+        finally
+        {
+            ta.recycle();
+        }
+    }
+    private void assignUiElements()
+    {
+        // layout is inflated, assign local variables to components
+        header = (LinearLayout)findViewById(R.id.calendar_header);
+        btnPrev = (ImageView)findViewById(R.id.calendar_prev_button);
+        btnNext = (ImageView)findViewById(R.id.calendar_next_button);
+        txtDate = (TextView)findViewById(R.id.calendar_date_display);
+        grid = (GridView)findViewById(R.id.calendar_grid);
+    }
+
+    private void assignClickHandlers()
+    {
+        // add one month and refresh UI
+        btnNext.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                currentDate.add(Calendar.MONTH, 1);
+                updateCalendar();
+            }
+        });
+
+        // subtract one month and refresh UI
+        btnPrev.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                currentDate.add(Calendar.MONTH, -1);
+                updateCalendar();
+            }
+        });
+
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (eventHandler != null) {
+                    eventHandler.onDayPress((Date)parent.getItemAtPosition(position));
+                }
+            }
+        });
+    }
+
+    /**
+     * Display dates correctly in grid
+     */
+    public void updateCalendar()
+    {
+        updateCalendar(null);
+    }
+
+    /**
+     * Display dates correctly in grid
+     */
+    public void updateCalendar(HashSet<Date> events)
+    {
+        ArrayList<Date> cells = new ArrayList<>();
+        Calendar calendar = (Calendar)currentDate.clone();
+
+        // determine the cell for current month's beginning
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        int monthBeginningCell = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+        // move calendar backwards to the beginning of the week
+        calendar.add(Calendar.DAY_OF_MONTH, -monthBeginningCell);
+
+        // fill cells
+        while (cells.size() < DAYS_COUNT)
+        {
+            cells.add(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        // update grid
+        grid.setAdapter(new CalendarAdapter(getContext(), cells, events));
+
+        // update title
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        txtDate.setText(sdf.format(currentDate.getTime()));
+    }
+
+
+    private class CalendarAdapter extends ArrayAdapter<Date>
+    {
+        // days with events
+        private HashSet<Date> eventDays;
+
+        // for view inflation
+        private LayoutInflater inflater;
+
+        public CalendarAdapter(Context context, ArrayList<Date> days, HashSet<Date> eventDays)
+        {
+            super(context, R.layout.control_calendar_day, days);
+            this.eventDays = eventDays;
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent)
+        {
+            // day in question
+            Date date = getItem(position);
+            int day = date.getDate();
+            int month = date.getMonth();
+            int year = date.getYear();
+
+            // today
+            Date today = new Date();
+
+            // inflate item if it does not exist yet
+            if (view == null)
+                view = inflater.inflate(R.layout.control_calendar_day, parent, false);
+
+
+            // clear styling
+            ((TextView)view).setTypeface(null, Typeface.NORMAL);
+            ((TextView)view).setTextColor(Color.BLACK);
+
+            if (month != today.getMonth() || year != today.getYear())
+            {
+                // if this day is outside current month, grey it out
+                ((TextView)view).setTextColor(getResources().getColor(R.color.greyed_out));
+            }
+            else if (day == today.getDate())
+            {
+                // if it is today, set it to blue/bold
+                ((TextView)view).setTypeface(null, Typeface.BOLD);
+                ((TextView)view).setTextColor(getResources().getColor(R.color.today));
+            }
+
+            // set text
+            ((TextView)view).setText(String.valueOf(date.getDate()));
+
+            return view;
+        }
+    }
+
+    /**
+     * Assign event handler to be passed needed events
+     */
+    public void setEventHandler(EventHandler eventHandler)
+    {
+        this.eventHandler = eventHandler;
+    }
+
+    /**
+     * This interface defines what events to be reported to
+     * the outside world
+     */
+    public interface EventHandler
+    {
+        void onDayPress(Date date);
+    }
+}
---
+++ app/src/main/java/com/todo/todolist/MainActivity.java
@@ -0,0 +1,46 @@
+package com.todo.todolist;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 액션 바 제거
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // 달력 구성
+        HashSet<Date> events = new HashSet<Date>();
+        events.add(new Date());
+
+        CustomCalendarView mainCal = (CustomCalendarView) findViewById(R.id.mainCal);
+        mainCal.updateCalendar(events);
+
+        mainCal.setEventHandler(new CustomCalendarView.EventHandler() {
+            @Override
+            public void onDayPress(Date date) {
+                DateFormat df = SimpleDateFormat.getDateInstance();
+                Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
